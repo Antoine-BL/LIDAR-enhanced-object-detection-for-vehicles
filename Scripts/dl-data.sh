1f1a82f0e87fc2a011b3053f5637c4832673ec97
antoinebrassardlahey <antoine.brassard@gmail.com>
2022-06-19 16:07:12 -0400

    Pretreat for darknet

--- Scripts/dl-data.sh
+++ Scripts/dl-data.sh
@@ -3,7 +3,7 @@ project_id='splendid-binder-350320'
 gcloud config set project $project_id
 
 # Download training data
-for i in $(seq -f "%04g" 0 31)
+for i in $(seq -f "%04g" 16 31)
 do
     file_name=training_$i.tar
     gsutil cp gs://waymo_open_dataset_v_1_1_0/training/$file_name  ../Data/training/tar/$file_name
